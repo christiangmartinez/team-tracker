c1f8851b7180bd835a06728b2ea355daf5357493
Christian Martinez <cgmartinez11142@gmail.com>
2017-03-24 09:42:06 -0700

    add methods and tests to Member class to assign and find members by Id

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,5 +1,5 @@
 import java.util.List;
-import java.util.ArrayList
+import java.util.ArrayList;
 
 public class Member {
   private String mName;
@@ -7,6 +7,7 @@ public class Member {
   private String mSpecialty;
   private String mHome;
   private static List<Member> instances = new ArrayList<Member>();
+  private int mId;
 
   public Member(String name, String work, String specialty, String home) {
     mName = name;
@@ -14,6 +15,7 @@ public class Member {
     mSpecialty = specialty;
     mHome = home;
     instances.add(this);
+    mId = instances.size();
   }
 
   public String getName() {
@@ -30,6 +32,22 @@ public class Member {
 
   public String getHome() {
     return mHome;
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static List<Member> all() {
+    return instances;
+  }
+
+  public static void clear () {
+    instances.clear();
+  }
+
+  public static Member find(int id) {
+    return instances.get(id - 1);
   }
 
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -3,6 +3,11 @@ import static org.junit.Assert.*;
 
 public class MemberTest {
 
+  @Before
+  public void tearDown() {
+    Member.clear();
+  }
+
   @Test
   public void member_instantiatesCorrectly_true() {
     Member testMember = new Member("Chulo", "Chulo's house", "Being a dog", "Portland");
@@ -16,6 +21,29 @@ public class MemberTest {
     assertEquals("Chulo's house", testMember.getWork());
     assertEquals("Being a dog", testMember.getSpecialty());
     assertEquals("Portland", testMember.getHome());
+    assertEquals(1 , testMember.getId());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfMember_true() {
+    Member firstMember = new Member("Chulo", "Chulo's house", "Being a dog", "Portland");
+    Member secondMember = new Member("Valentina", "Valentina's house", "Being a cat", "Portland");
+    assertTrue(Member.all().contains(firstMember));
+    assertTrue(Member.all().contains(secondMember));
+  }
+
+  @Test
+  public void clear_emptiesList_0() {
+    Member testMember = new Member("Chulo", "Chulo's house", "Being a dog", "Portland");
+    Member.clear();
+    assertEquals(0, Member.all().size());
+  }
+
+  @Test
+  public void find_returnsMemberbyId_secondMember() {
+    Member firstMember = new Member("Chulo", "Chulo's house", "Being a dog", "Portland");
+    Member secondMember = new Member("Valentina", "Valentina's house", "Being a cat", "Portland");
+    assertEquals(secondMember, Member.find(secondMember.getId()));
   }
 
 }
