aab8c3f092a529aa580e6816045139a4f7f63c20
Christian Martinez <cgmartinez11142@gmail.com>
2017-03-24 16:34:19 -0700

    add css and page for info about event

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,6 +15,12 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/about", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/about.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     get("/teams", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("teams", Team.all());
